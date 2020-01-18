32e81abe794340a1bad45184177c095c06f40b15
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-18 21:08:47 +0530

    adding config to heroku

--- index.js
+++ index.js
@@ -25,6 +25,4 @@ app.get("/", (req, res) => {
   res.json({ message: "welcome to crwn-db" });
 });
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`server started at port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
